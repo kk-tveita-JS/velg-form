b43c0d4e17540d70d942c794499b538d2f460f68
kk-tveita-JS <91045562+kk-tveita-JS@users.noreply.github.com>
2021-09-20 22:10:43 +0200

    Oppdater pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,11 +3,28 @@ input.onButtonPressed(Button.A, function () {
     if (Figur % 4 == 0) {
         Figur = 0
     }
-    basic.showNumber(Figur)
 })
 let Figur = 0
 bitbot.select_model(BBModel.XL)
 Figur = 0
 basic.forever(function () {
-	
+    if (Figur == 0) {
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            # . . . #
+            . # . # .
+            . . # . .
+            `)
+    } else if (Figur == 1) {
+        basic.showLeds(`
+            . # # # .
+            # . . . #
+            # . . . #
+            # . . . #
+            . # # # .
+            `)
+    } else {
+        basic.showNumber(Figur)
+    }
 })
