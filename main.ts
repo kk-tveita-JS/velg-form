5a2690529b19708d58e61b2aa79e01d6cb27c652
kk-tveita-JS <91045562+kk-tveita-JS@users.noreply.github.com>
2021-09-21 19:34:31 +0200

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,11 @@
 input.onButtonPressed(Button.A, function () {
-    Figur = Figur + 1
+    Figur += 1
     if (Figur % 4 == 0) {
         Figur = 0
     }
+    basic.showNumber(Figur)
 })
-let Figur = 0
-bitbot.select_model(BBModel.XL)
-Figur = 0
-basic.forever(function () {
+input.onButtonPressed(Button.B, function () {
     if (Figur == 0) {
         basic.showLeds(`
             . . # . .
@@ -36,3 +34,6 @@ basic.forever(function () {
         basic.showNumber(Figur)
     }
 })
+let Figur = 0
+Figur = 0
+basic.showNumber(Figur)
